557d98e3aa4c86a6931d8ae3449f64ca232c043c
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2017-11-12 22:34:33 +0700

    Fix bugs

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const cleanPattern = /(<(section-|include-file|extend-to)([^>]+)>|(<\/(section-[
 
 module.exports = () => {
 
+    let translatedTemplates = {};
+
     function readFile(fpath) {
         return fs.readFileSync(fpath).toString();
     }
@@ -41,11 +43,117 @@ module.exports = () => {
         templateDoc = parser.parseFromString(readFile(templateFilePath), 'text/html');
         sectionTemplate = templateDoc.getElementsByTagName(`section-${sectionAttr}`);
         
-        if (!sectionElement.length || !sectionTemplate.length) return templateDoc;
+        if (sectionElement.length && sectionTemplate.length) {
+            sectionTemplate[0].appendChild(sectionElement[0]);
+        }
 
-        sectionTemplate[0].appendChild(sectionElement[0]);
+        return {
+            doc: templateDoc,
+            path: templateFilePath
+        };
+    }
 
-        return templateDoc;
+    function translateIncludeFile(basedir, parsedContent) {
+        let includeFileElements = parsedContent.getElementsByTagName('include-file');
+
+        for(let i=0,length=includeFileElements.length;i<length;i++) {
+            let includeFileElement = includeFileElements[i],
+                includeFile, includeFilePath;
+
+            if (!includeFileElement.hasAttribute('src')) continue;
+            includeFilePath = path.resolve(basedir, includeFileElement.getAttribute('src'));
+            
+            if (isFile(includeFilePath)) {
+                let fileToInclude = readFile(includeFilePath),
+                    fileIncludeDoc = parser.parseFromString(fileToInclude);
+
+                // Make sure this is a html source
+                if (fileIncludeDoc) {
+                    includeFileElement.appendChild(fileIncludeDoc);
+                }
+            }
+        }
+
+        return parsedContent;
+    }
+
+    function appendSectionToTemplate(sectionElement, templateFilePath, sectionSelector) {
+        let templateDoc;
+
+        if (!translatedTemplates[templateFilePath]) {
+            templateDoc = parser.parseFromString(readFile(templateFilePath));
+            translatedTemplates[templateFilePath] = templateDoc;
+        } else {
+            return true;
+        }
+
+        // Make sure templateDoc is parsed as html
+        if (templateDoc) {
+            templateDoc = translateIncludeFile(path.dirname(templateFilePath), templateDoc);
+            
+            let sectionTarget = templateDoc.getElementsByTagName(sectionSelector);
+            
+            if (sectionTarget.length) {
+                sectionTarget[0].appendChild(sectionElement);
+                return templateDoc;
+            }
+        }
+
+        return false;
+    }
+
+    function translateExtendElement(parsedContent, filePath) {
+        let basedir = path.dirname(filePath),
+            extendElements = parsedContent.getElementsByTagName('extend-to');
+
+        // Support for multiple section
+        for(let i=0,length=extendElements.length;i<length;i++) {
+            let extendElement = extendElements[i], sectionTarget, 
+                templateFilePath, sectionElement;
+            
+            // Check for src and section attribute
+            if (extendElement.hasAttribute('src') && extendElement.hasAttribute('section')) {
+                sectionTarget = 'section-' + extendElement.getAttribute('section');
+                templateFilePath = path.resolve(basedir, extendElement.getAttribute('src'));
+                sectionElement = parsedContent.getElementsByTagName(sectionTarget);
+
+                if (sectionElement.length && isFile(templateFilePath)) {
+                    let doc = appendSectionToTemplate(sectionElement[0], templateFilePath, sectionTarget);
+                    if (doc !== false) {
+                        extendElement.childNodes = new Array();
+                        extendElement.appendChild(doc);
+                    } 
+
+                    // Already translated template
+                    if (doc === true) {
+                        sectionTarget = parsedContent.getElementsByTagName(sectionTarget);
+                        for (let j=0,len=sectionTarget.length;j<len;j++) {
+                            if (sectionTarget[j] != sectionElement[0]) {
+                                let appendableElement, childElement;
+                                
+                                if (sectionTarget[j].childNodes.length > 0)  {
+                                    appendableElement = sectionElement[0];
+                                    childElement = sectionTarget[j];
+                                } else {
+                                    appendableElement = sectionTarget[j];
+                                    childElement = sectionElement[0];
+                                }
+
+                                appendableElement.appendChild(childElement);
+                                extendElement.childNodes = new Array();
+
+                                // I don't know why this give me a little
+                                // bit confusing with return `??`
+                                parsedContent.removeChild(extendElement);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return parsedContent;
     }
 
     return through.obj((file, enc, cb) => {
@@ -66,49 +174,19 @@ module.exports = () => {
 
         let parsedContent = parser.parseFromString(file.contents.toString(), 'text/html');
         
+        // Non html
         if (!parsedContent) {
             return cb(null, file)
         }
 
-        let basedir, templateDoc, extendElements;
+        let templateDoc, xmlSerializeString;
 
-        extendElements = parsedContent.getElementsByTagName('extend-to')
-        basedir = path.dirname(path.resolve(file.path));
+        templateDoc = translateIncludeFile(path.dirname(file.path), parsedContent);
+        templateDoc = translateExtendElement(templateDoc, file.path);
 
-        // If have extend element
-        if (extendElements.length) {
-            if (extendElements[0].hasAttribute('src') || extendElements[0].hasAttribute('section')) {
-                templateDoc = doExtend(parsedContent, extendElements[0], file.path);
-            } else {
-                templateDoc = parsedContent;
-            }
-        }  else {
-            templateDoc = parsedContent;
-        }
-
-        if (templateDoc) {
-            let includeElements = templateDoc.getElementsByTagName('include-file');
-
-            for(let i=0,length=includeElements.length;i<length;i++) {
-                let includeElement = includeElements[i],
-                    src = includeElement.getAttribute('src'),
-                    includeFilePath = path.resolve(basedir, src),
-                    includeFileDoc;
-
-                if (!src.length) continue;
-                if (!isFile(includeFilePath)) {
-                    gutil.log(gutil.colors.red(`File ${path.basename(includeFilePath)} not found`), 'on', gutil.colors.bold(path.basename(file.path)));
-                    continue;
-                }
-
-                includeFileDoc = parser.parseFromString(readFile(includeFilePath), 'text/html');
-                includeElement.appendChild(includeFileDoc);
-            }
-        }
-
-        let content =  (!templateDoc) ? file.contents.toString() : new dom.XMLSerializer().serializeToString(templateDoc);
-        content = content.replace(cleanPattern,'');
-        file.contents = new Buffer(content); 
+        xmlSerializeString = new dom.XMLSerializer().serializeToString(templateDoc);
+        
+        file.contents = new Buffer(xmlSerializeString.trim().replace(cleanPattern, ''));
         
         return cb(null, file);
     });
